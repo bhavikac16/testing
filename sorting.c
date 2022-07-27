8cd962faa83f3147643df37b9aa7d9966bcb5929
Bhavika Choudhary <69667567+bhavikac16@users.noreply.github.com>
2022-07-27 21:04:34 +0530

    Add files via upload

--- sorting.c
+++ sorting.c
@@ -1,1 +1,58 @@
 #include <stdio.h>
+
+int swap;
+
+  
+// Bubble sort
+void bubbleSort(int array[], int n)
+{
+    for (int c = 0 ; c < n - 1; c++){
+        for (int d = 0 ; d < n - c - 1; d++)
+        {
+          if (array[d] > array[d+1]) /* For decreasing order use '<' instead of '>' */
+          {
+            swap       = array[d];
+            array[d]   = array[d+1];
+            array[d+1] = swap;
+          }
+        }
+    }
+
+    printf("Sorted list in ascending order:\n");
+
+    for (int c = 0; c < n; c++)
+        printf("%d ", array[c]);
+}
+  
+
+
+int main(){
+ 
+
+    int i, j, a, n, number[30];
+
+    printf("Enter the value of N \n");
+    scanf("%d", &n);
+    
+    printf("Enter the numbers \n");
+
+    for (i = 0; i < n; ++i)
+        scanf("%d", &number[i]);
+
+    printf("Choose the type of sorting \n 1.Insertion sort\n 2.Selection sort\n 3.Bubble sort\n 4.Merge Sort\n 5.Quick sort\n");
+    int x;
+    scanf("%d",&x);
+        
+    
+    switch(x){
+        case 3:
+            bubbleSort(number, n);
+            
+            break;
+
+        default :
+            printf("Error! Chosen option not given");   
+            
+    }
+    return 0;
+}
